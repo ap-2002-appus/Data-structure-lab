e6b6fd67279d951a9c3e9bc9bbaa0139410b6246
Ibin Siby <ibinsiby4619@gmail.com>
2025-07-30 20:30:30 +0530

    Update add_poly.c

--- add_poly.c
+++ add_poly.c
@@ -1,76 +1,85 @@
 #include <stdio.h>
 
-int main() {
-    int r1, c1, n1,r2, c2, n2,i;
-    printf("Enter rows, columns and number of non-zero elements of Matrix 1: ");
-    scanf("%d %d %d", &r1, &c1, &n1);
-    int mat1[3][100];
-    printf("Enter the elements (row column value) of Matrix 1:\n");
-    for(i = 0; i < n1; i++) {
-        scanf("%d %d %d", &mat1[0][i], &mat1[1][i], &mat1[2][i]);
+#define MAX 100
+
+
+typedef struct {
+    int coeff; // Coefficient
+    int exp;   // Exponent
+} Term;
+
+
+void inputPoly(Term poly[], int *n) {
+    printf("Enter number of terms: ");
+    scanf("%d", n);
+
+    for (int i = 0; i < *n; i++) {
+        printf("Enter term %d (coefficient and exponent): ", i + 1);
+        scanf("%d %d", &poly[i].coeff, &poly[i].exp);
     }
-    printf("Enter rows, columns and number of non-zero elements of Matrix 2: ");
-    scanf("%d %d %d", &r2, &c2, &n2);
-    int mat2[3][100];
-    printf("Enter the elements (row column value) of Matrix 2:\n");
-    for(i = 0; i < n2; i++) {
-        scanf("%d %d %d", &mat2[0][i], &mat2[1][i], &mat2[2][i]);
+}
+
+
+void displayPoly(Term poly[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%dx^%d", poly[i].coeff, poly[i].exp);
+        if (i < n - 1)
+            printf(" + ");
     }
-    if (r1 != r2 || c1 != c2) {
-        printf("Error: Matrices dimensions do not match for addition.\n");
-        return 1;
-    }
-    printf("\nSparse Matrix 1 (row column value):\n");
-    for(i = 0; i < n1; i++) {
-        printf("%d %d %d\n", mat1[0][i], mat1[1][i], mat1[2][i]);
-    }
-    printf("\nSparse Matrix 2 (row column value):\n");
-    for(i = 0; i < n2; i++) {
-        printf("%d %d %d\n", mat2[0][i], mat2[1][i], mat2[2][i]);
-    }
-    int res[3][200];
-    int i1 = 0, i2 = 0, k = 0;
-    while(i1 < n1 && i2 < n2) {
-        int r1_pos = mat1[0][i1];
-        int c1_pos = mat1[1][i1];
-        int r2_pos = mat2[0][i2];
-        int c2_pos = mat2[1][i2];
-        if (r1_pos < r2_pos || (r1_pos == r2_pos && c1_pos < c2_pos)) {
-            res[0][k] = r1_pos;
-            res[1][k] = c1_pos;
-            res[2][k] = mat1[2][i1];
-            i1++; k++;
-        } else if (r2_pos < r1_pos || (r2_pos == r1_pos && c2_pos < c1_pos)) {
-            res[0][k] = r2_pos;
-            res[1][k] = c2_pos;
-            res[2][k] = mat2[2][i2];
-            i2++; k++;
+    printf("\n");
+}
+
+
+int addPoly(Term poly1[], int n1, Term poly2[], int n2, Term result[]) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1 && j < n2) {
+        if (poly1[i].exp == poly2[j].exp) {
+            result[k].coeff = poly1[i].coeff + poly2[j].coeff;
+            result[k].exp = poly1[i].exp;
+            i++;
+            j++;
+        } else if (poly1[i].exp > poly2[j].exp) {
+            result[k] = poly1[i];
+            i++;
         } else {
-            int summed = mat1[2][i1] + mat2[2][i2];
-            if (summed != 0) { 
-                res[0][k] = r1_pos;
-                res[1][k] = c1_pos;
-                res[2][k] = summed;
-                k++;
-            }
-            i1++; i2++;
+            result[k] = poly2[j];
+            j++;
         }
+        k++;
     }
-    while(i1 < n1) {
-        res[0][k] = mat1[0][i1];
-        res[1][k] = mat1[1][i1];
-        res[2][k] = mat1[2][i1];
-        i1++; k++;
+
+    // Copy remaining terms
+    while (i < n1) {
+        result[k++] = poly1[i++];
     }
-    while(i2 < n2) {
-        res[0][k] = mat2[0][i2];
-        res[1][k] = mat2[1][i2];
-        res[2][k] = mat2[2][i2];
-        i2++; k++;
+    while (j < n2) {
+        result[k++] = poly2[j++];
     }
-    printf("\nSum of Sparse Matrices (row column value):\n");
-    for(i = 0; i < k; i++) {
-        printf("%d %d %d\n", res[0][i], res[1][i], res[2][i]);
-    }
+
+    return k; 
+}
+
+int main() {
+    Term poly1[MAX], poly2[MAX], sum[MAX];
+    int n1, n2, nSum;
+
+    printf("Enter first polynomial:\n");
+    inputPoly(poly1, &n1);
+
+    printf("\nEnter second polynomial:\n");
+    inputPoly(poly2, &n2);
+
+    nSum = addPoly(poly1, n1, poly2, n2, sum);
+
+    printf("\nFirst Polynomial: ");
+    displayPoly(poly1, n1);
+
+    printf("Second Polynomial: ");
+    displayPoly(poly2, n2);
+
+    printf("Sum Polynomial: ");
+    displayPoly(sum, nSum);
+
     return 0;
 }
